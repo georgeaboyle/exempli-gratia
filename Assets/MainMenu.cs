5cbdc20fb6e7e6e93c79825cea0dc590a3db7af3
georgeaboyle <georgeaboyle@gmail.com>
2024-03-07 07:57:13 +0200

    Update MainMenu.cs

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,11 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame ()
-    {
+   // public void QuitGame ()
+   // {
         // I don't need this for this build since it will be played in a browser window
         // However, I have included it in case I want to add a quit button to the menu for stand-alone builds
         // Basically, just ignore this. It's here for me, not for you.
-        Application.Quit();
-    }
+        //Application.Quit();
+    //}
 }
