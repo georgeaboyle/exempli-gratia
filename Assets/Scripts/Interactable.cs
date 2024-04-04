ce64b342179c91e95b316bad037148ea04145c10
georgeaboyle <georgeaboyle@gmail.com>
2024-04-04 12:12:24 +0300

    New Living room and Inventory Code begins
    Began working on a new living room with actual assets
    Began coding in inventory system and interactable items
    Should have done this on different branches, but I had a brain fart, and did them together. Will branch off after this push.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    bool isFocus = false;
+    Transform player;
+
+    void Update()
+    {
+        if (isFocus)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius)
+            {
+                Debug.Log("INTERACT");
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+    }
+}
