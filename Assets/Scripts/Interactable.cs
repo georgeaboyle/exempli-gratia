a8510ae1ea6e3815ca9dde4e8d46540c5e52109f
georgeaboyle <georgeaboyle@gmail.com>
2024-04-12 00:43:16 +0300

    Inventory (pretty much) complete
    For some reason, the console says that I'm always picking up seeds, but items disappear from the world and reappear in inventory when right clicked, so I call that a win for tonight.

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -16,17 +16,17 @@ public class Interactable : MonoBehaviour
         // This method is meant to be overridden by scripts in other places
         // This is why it is "virtual"
 
-        Debug.Log("Interacting with " + transform.name);
+        Debug.Log("I say, I say, I'm Interacting with " + transform.name);
     }
 
     void Update()
     {
         if (isFocus && !hasInteracted)
         {
-            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            // float distance = Vector3.Distance(player.position, interactionTransform.position);
             Interact();
-            Debug.Log("INTERACT");
             hasInteracted = true;
+            Debug.Log("INTERACT");
             //if (distance <= radius)
             //{
             //    Interact();
@@ -53,13 +53,13 @@ public class Interactable : MonoBehaviour
     // I don't know if we need this at all now that we aren't using the radius...
     // I'll add it back in if I find it's necessary
 
-    //private void OnDrawGizmosSelected()
-    //{
-    //    if (interactionTransform == null)
-    //        interactionTransform = transform;
+    private void OnDrawGizmosSelected ()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
 
-    //    Gizmos.color = Color.yellow;
-    //    Gizmos.DrawWireSphere(interactionTransform.position, radius);
+        //gizmos.color = color.yellow;
+        //gizmos.drawwiresphere(interactionTransform.position, radius);
 
-    //}
+    }
 }
